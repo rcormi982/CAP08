f5281e817be522620861f49005c4157658d52508
Rocio <rociocor77@gmail.com>
2024-01-07 19:21:56 +0100

    EJ20BIS2

---
+++ EJ20BIS.java
@@ -0,0 +1,7 @@
+public class EJ20BIS {
+    public static void muestraArrayInt(int[] n) {
+        for (int i = 0; i < n.length; i++) {
+            System.out.print(n[i] +  " ");
+        }
+    }
+}
