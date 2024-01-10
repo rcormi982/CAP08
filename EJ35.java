7f280ed9f50b4647314e3e52c5d3d8788aaa090c
Rocio <rociocor77@gmail.com>
2024-01-10 19:15:59 +0100

    EJ35

---
+++ EJ35.java
@@ -0,0 +1,24 @@
+import static Matematicas.Varias.digitos;
+import static Matematicas.Varias.digitoN;
+public class EJ35 {
+    public static String convierteEnPalotes(int n) {
+        String resultado = "";
+        for(int i = 0; i < digitos(n); i++) {
+            for (int j = 0; j < digitoN(n, i); j++) {
+                resultado += "|";
+            }
+            if (i < digitos(n) - 1) {
+                resultado += "-";
+            }
+        }
+        return resultado;
+    }
+    public static void main(String[] args) {
+        int[] numeros = {470213, 0, 11, 24, 867024};
+        for (int numero : numeros) {
+            System.out.print("El " + numero + " es el " + convierteEnPalotes(numero));
+            System.out.println(" en el sistema de palotes.");
+        }
+    }
+}
+        
